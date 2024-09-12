251fc4d088bcdc3c6fe8adef0158a42ec0d9f0a2
Ujjwaltyagi295 <145588431+Ujjwaltyagi295@users.noreply.github.com>
2024-09-13 00:45:25 +0530

    i

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* Wrap App in BrowserRouter */}
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
